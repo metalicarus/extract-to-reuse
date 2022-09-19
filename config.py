c37dad90c140d3437b900c2f9d80846031f0d9e1
Mateus Da Silva <silvamateus@brq.com>
2022-09-19 14:14:47 -0300

    vanilla construction of the application

---
+++ config.py
@@ -0,0 +1,36 @@
+from pytesseract import pytesseract
+from sys import platform
+from os.path import exists
+import json
+import logging
+logging.basicConfig(filename='application.log', encoding='utf-8', level=logging.DEBUG )
+
+class Config:
+
+    def __init__(self):
+        logging.info('[CONFIG][__init__]')
+        file = self.__getConfigFile()
+        path = file['tesseract']['path'][platform] + file['tesseract']['file'][platform]
+
+        if (platform == 'win32'):
+            logging.info('[PLATFORM]=win32')
+            logging.info(path)
+            check = self.__checkPathToTesseract(path)
+            if (check == False):
+                logging.error("tesseract was not found")
+                exit()
+        else:
+            logging.info('[PLATFORM]=linux')
+        
+        self.__path_to_tesseract = path
+
+    def __checkPathToTesseract(self, path: str):
+        return exists(path)
+
+    def __getConfigFile(self):
+        return json.load(open("params.json"))
+    
+    def getPytesseract(self) -> pytesseract:
+        print(self.__path_to_tesseract)
+        pytesseract.tesseract_cmd = self.__path_to_tesseract
+        return pytesseract
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from sys import platform
+from config import Config
+import logging
 from PIL import Image
-from pytesseract import pytesseract
 
-path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+logging.basicConfig(filename='application.log', encoding='utf-8', level=logging.DEBUG )
+
+
+
+# path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 path_to_image = 'Caras-08-10.jpg'
 
-pytesseract.tesseract_cmd = path_to_tesseract
+# pytesseract.tesseract_cmd = path_to_tesseract
 img = Image.open(path_to_image)
 
+# text = pytesseract.image_to_string(img)
+
+
+
+logging.info('[APPLICATION][START]')
+config = Config()
+pytesseract = config.getPytesseract()
 text = pytesseract.image_to_string(img)
 
 file = open('texto.txt', 'a')
