735fa36bd069caee9a86eec01e9a6168530c2e27
Mateus Da Silva <silvamateus@brq.com>
2022-09-19 14:19:29 -0300

    __init__ constructor

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from PIL import Image
 
 logging.basicConfig(filename='application.log', encoding='utf-8', level=logging.DEBUG )
 
+def __init__():
+    # path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    path_to_image = 'Caras-08-10.jpg'
+
+    # pytesseract.tesseract_cmd = path_to_tesseract
+    img = Image.open(path_to_image)
+
+    # text = pytesseract.image_to_string(img)
 
 
-# path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-path_to_image = 'Caras-08-10.jpg'
 
-# pytesseract.tesseract_cmd = path_to_tesseract
-img = Image.open(path_to_image)
+    logging.info('[APPLICATION][START]')
+    config = Config()
+    pytesseract = config.getPytesseract()
+    text = pytesseract.image_to_string(img)
 
-# text = pytesseract.image_to_string(img)
+    file = open('texto.txt', 'a')
+    file.write(text)
+    file.close()
 
-
-
-logging.info('[APPLICATION][START]')
-config = Config()
-pytesseract = config.getPytesseract()
-text = pytesseract.image_to_string(img)
-
-file = open('texto.txt', 'a')
-file.write(text)
-file.close()
+if __name__ == '__main__':
+    __init__()
